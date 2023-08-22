fc1a20875e7e7bbaa8cfbc2413d75d883473f1e3
Rithvik Vellaturi <vikturi99@gmail.com>
2023-08-22 00:22:13 -0500

    upload to s3 endpoint

--- fastapi-react/backend/app/api.py
+++ fastapi-react/backend/app/api.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import boto3
+import os
+from dotenv import load_dotenv
+from fastapi import File,UploadFile
 
+load_dotenv()
+
+session = boto3.Session(
+    aws_access_key_id= os.getenv('REACT_APP_ACCESSKEYID'),
+    aws_secret_access_key=os.getenv('REACT_APP_SECRETACCESSKEY'),
+)
+s3_client = boto3.client('s3')
 
 app = FastAPI()
 
@@ -22,3 +33,8 @@ app.add_middleware(
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to postex api."}
+
+@app.post("/uploadFile")
+async def upload_file(file_data: UploadFile = File(...)):
+    s3_client.upload_fileobj(file_data.file, os.getenv('REACT_APP_BUCKETNAME'), file_data.filename)
+    return {"message": "File uploaded successfully"}
